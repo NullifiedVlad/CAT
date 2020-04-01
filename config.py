c945adc2d6a521cc15367efa35d2663237d0233c
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-04-01 23:12:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 token = '' #Вставьте токен бота
 channel =  #вставьте id канала
 helpmessage = '''
--=Configuration Administrarion Tool=-
-!move (X) (Y) (time) - передвинуть курсор.
-!click - сделать левый клик мышкой.
-!write (message) - напечатать что-то.
-!send (how_many) - нажать клавишу несколько раз.
-!screenshot - сделать скриншот экрана.
-!playsound (1-2) - проиграть звук.
-!changelanguage - изменить раскладку клавиатуры.
-!help - выводит это сообщение.
-!processlist - выводит список процессов.
-!command - выполнить комманду в терминале.
-!kill (process) - закрыть принудительно процесс.
+-=**Configuration Administrarion Tool**=-
+**!move** (X) (Y) (time) - передвинуть курсор.
+**!click** - сделать левый клик мышкой.
+**!write** (message) - напечатать что-то.
+**!send** (how_many) - нажать клавишу несколько раз.
+**!screenshot** - сделать скриншот экрана.
+**!playsound** (1-2) - проиграть звук.
+**!changelanguage** - изменить раскладку клавиатуры.
+**!help** - выводит это сообщение.
+**!processlist** - выводит список процессов.
+**!command** - выполнить комманду в терминале.
+**!kill** (process) - закрыть принудительно процесс.
+**!video** -захват видео
 -==== Made by NullifiedVlad ====-'''
