610d9c5a766b0dea316933e0df09a8beafbc2001
NullifiedVlad <alpatof74@gmail.com>
2020-05-08 15:15:08 +0300

    update

--- CAT.py
+++ CAT.py
@@ -77,7 +77,7 @@ async def press(ctx, key, how_many):  # нажать клавишу
 
 @bot.command()
 async def help(ctx):  # сообщение помощи
-    await ctx.send(config.helpmessage, file=discord.File('Banners/Banner.png'))
+    await ctx.send(config.helpmessage)
 
 
 @bot.command()
